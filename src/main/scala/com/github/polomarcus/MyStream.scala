da220cd884ad6eefa2a9fdc7a26e1fc6d5db64d8
Paul Leclercq <paul.leclercq@tabmo.io>
2018-11-19 10:51:08 +0100

    feat: stream uses config and improve log

--- src/main/scala/com/github/polomarcus/MyStream.scala
+++ src/main/scala/com/github/polomarcus/MyStream.scala
@@ -3,6 +3,7 @@ package com.github.polomarcus
 import java.util.Properties
 import java.util.concurrent.TimeUnit
 
+import com.github.polomarcus.conf.ConfService
 import com.typesafe.scalalogging.Logger
 import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.scala._
@@ -13,25 +14,25 @@ import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
   * From https://kafka.apache.org/20/documentation/streams/developer-guide/dsl-api.html#scala-dsl
   */
 object WordCountApplication extends App {
-  val logger = Logger("WordCount")
+  val logger = Logger(WordCountApplication.getClass)
 
   import Serdes._
 
   val props: Properties = {
     val p = new Properties()
     p.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application")
-    p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, ConfService.BOOTSTRAP_SERVERS_CONFIG)
     p
   }
 
   val builder: StreamsBuilder = new StreamsBuilder
-  val textLines: KStream[String, String] = builder.stream[String, String]("TextLinesTopic")
+  val textLines: KStream[String, String] = builder.stream[String, String](ConfService.TOPIC_IN)
   val wordCounts: KTable[String, Long] = textLines
     .flatMapValues(textLine => textLine.toLowerCase.split("\\W+"))
     .groupBy((_, word) => word)
     .count()
 
-  wordCounts.toStream.to("WordsWithCountsTopic")
+  wordCounts.toStream.to(ConfService.TOPIC_OUT)
 
   val streams: KafkaStreams = new KafkaStreams(builder.build(), props)
 
